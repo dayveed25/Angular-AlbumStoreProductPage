790c7c4c6dd7fd52b82798466dddf84623300c73
dayveed25 <day_veed@ymail.com>
2020-09-24 21:51:17 +0100

    Product Description

---
+++ src/app/product.service.ts
@@ -0,0 +1,11 @@
+import { Injectable } from '@angular/core';
+import { Http } from '@angular/http';
+import { from } from 'rxjs/observable/from';
+import { Response } from '@angular/http';
+
+@Injectable()
+export class ProductService {
+
+  constructor() { }
+
+}
