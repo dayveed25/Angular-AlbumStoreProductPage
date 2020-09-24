8d2f22bc6e33bc5b7a26885bbe43812506191306
dayveed25 <day_veed@ymail.com>
2020-09-24 22:17:01 +0100

    Product Description

--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -1,11 +1,16 @@
 import { Injectable } from '@angular/core';
-import { Http } from '@angular/http';
+import { Http, Response } from '@angular/http';
 import { from } from 'rxjs/observable/from';
-import { Response } from '@angular/http';
+import 'rxjs/add/operator/map';
 
 @Injectable()
 export class ProductService {
 
-  constructor() { }
+  private _albumUrl = '../assets/album.json';
 
+  constructor(private_http: Http) { }
+
+  getAlbum (id: number){
+    return this._http.get(this._albumUrl).map((response) => response.json())
+  }
 }
